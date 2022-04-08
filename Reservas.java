fecdcad55a8c0b3e09c1b6908ee8a629b724c277
kalouseckJP <juanpablo.rodriguez@alumnos.ulagos.cl>
2022-04-08 10:58:57 -0400

    Ahora reservas tiene todos sus atributos

--- Reservas.java
+++ Reservas.java
@@ -11,10 +11,13 @@ public class Reservas{
 
     Reservas(int fecha, int horaInicio, int horaFin, int cantidad,int dinero, String planComida, String planDegustacion, String tipoReserva, String area){
         this.fecha = fecha;
+        this.horaInicio = horaInicio;
         this.horaFin = horaFin;
         this.cantidad = cantidad;
         this.planComida = planComida;
         this.dinero = dinero;
         this.planDegustacion = planDegustacion;
+        this.tipoReserva = tipoReserva;
+        this.area = area;
     }
 }
